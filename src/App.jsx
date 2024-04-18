df064a35c65fee8f99c35d63204d138dda8c0f2e
BrianQQQ <z1x2c37@gmail.com>
2024-04-17 20:26:43 -0400

    Add GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter } from "react-router-dom";
-
+import ReactGA from 'react-ga';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-BYQVN0R9PK'); // Replace 'YOUR_TRACKING_ID' with your actual GA tracking ID
+    // Record a pageview for the initial page load
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
